fd7ea484b1a462f2db8fbd0f13cb0f7cacdcca62
Ondřej Gajdušek <ogajduse@redhat.com>
2019-05-01 17:27:46 +0200

    Add Quality class

---
+++ src/cz/vutbr/feec/utko/mmum/project/Quality.java
@@ -0,0 +1,21 @@
+package cz.vutbr.feec.utko.mmum.project;
+
+public class Quality {
+
+    public double getMse(int[][] original, int[][] edited) {
+        int width = original.length;
+        int height = original[0].length;
+        double mse = 0;
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                mse += Math.pow(original[i][j] - edited[i][j], 2);
+            }
+        }
+        mse = mse / (width * height);
+        return mse;
+    }
+
+    public double getPsnr(int[][] original, int[][] edited) {
+        return 10 * Math.log10(Math.pow(255, 2) / getMse(original, edited));
+    }
+}
